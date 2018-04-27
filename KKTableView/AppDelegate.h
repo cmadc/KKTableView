c06eefc1ee2af43ab5101482a1a4fbc387e8009d
蔡明 <ming.cai@phicomm.com>
2018-04-27 09:52:48 +0800

    Initial Commit

---
+++ KKTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKTableView
+//
+//  Created by CaiMing on 2018/4/27.
+//  Copyright © 2018年 CaiMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
